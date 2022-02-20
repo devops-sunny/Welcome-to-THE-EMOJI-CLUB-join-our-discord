e1b5c23bcae685b477059ffed6449fc60a4b96ba
dhrupadpatel <dhrupad1912@gmail.com>
2022-02-20 12:58:54 +0530

    cursors

--- script.js
+++ script.js
@@ -33,3 +33,22 @@ TweenMax.to(".loader", 2.2, {
     top: "-100%",
     ease: Expo.easeInOut
 });
+
+
+
+// cursors
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+// end 
